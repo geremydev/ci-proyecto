5d29b31b90e4a5d6bf5b1aa20aaa79310558d6a5
GeremySmart <geremy.smartraveling@gmail.com>
2025-02-13 11:22:51 -0400

    Test updated

--- test/hello.test.js
+++ test/hello.test.js
@@ -3,8 +3,12 @@ test('debería retornar Hola Mundo por defecto', () => {
     expect(sayHello()).toBe('Hola, Mundo!!');
 });
 
-test('Debería retornar Hola, [Nombre personalizado]', () => {
+test('Debería retornar Hola, John!', () => {
     expect(sayHello('John')).toBe('Hola, John!');
+});
+
+test('Debería retornar Hola, Geremy!', () => {
+    expect(sayHello('Geremy')).toBe('Hola, Geremy!');
 });
 
 //Geremy Ferran 2022-2117
