e6c9ac6af622d36f165fb9462f1023f938e052e5
GeremySmart <geremy.smartraveling@gmail.com>
2025-02-13 11:09:05 -0400

    test updated

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,7 +1,7 @@
-const sayHello = require('../src/hello');
+const sayHello = require('../hello.js');
 
 test('debería retornar Hola Mundo por defecto', () => {
-    expect(sayHello()).toBe('Hola, Mundo!');
+    expect(sayHello()).toBe('Hola, Mundo!!');
 });
 
 test('Debería retornar Hola, [Nombre personalizado]', () => {
